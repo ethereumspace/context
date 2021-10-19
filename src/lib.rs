0dedb8855dcf5c8247f6f73ae65d9b8f7a42142f
ethereum <1532381694@qq.com>
2021-10-19 13:42:15 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use candid::utils::{ArgumentDecoder, ArgumentEncoder};
 use ic_cdk_macros::*;
 
 use ic_cdk_macros::*;
-#[update]
+
 pub async  fn createTransaction(id: Principal, method: &str) {
     ic_cdk::print(ic_cdk::caller().to_text());
     ic_cdk::call::<(),()>(id,method,()).await;
