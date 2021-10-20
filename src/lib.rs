75f4dd80170b20f57795d12e94cf9bc43471c911
ethereum <1532381694@qq.com>
2021-10-20 15:37:05 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,13 @@ pub mod generate {
     use super::Metadata;
     use std::result::Result;
 
-    pub fn crateRecode(data: &Metadata) -> Result<String, EventErr> {
-        let serial = serde_json::to_string(data).unwrap();
-        if serial.len() == 0 {
-            return Err(EventErr::SerializeFail);
-        }
-        Ok(serial)
-    }
+    // pub fn crateRecode(data: &Metadata) -> Result<String, EventErr> {
+    //     let serial = serde_json::to_string(data).unwrap();
+    //     if serial.len() == 0 {
+    //         return Err(EventErr::SerializeFail);
+    //     }
+    //     Ok(serial)
+    // }
 }
 
 pub async fn emit() -> () {
@@ -24,11 +24,11 @@ pub async fn emit() -> () {
     let caller = caller();
     let transaction_time = time();
     let stable_size = stable::stable_size();
-    let data = Metadata::new(&canister,&caller, transaction_time, stable_size as u64,10 as u64,"test");
+    let data = Metadata::new(&canister,&caller, transaction_time.into(), stable_size.into(),10.into(),"test");
 
-    let args= generate::crateRecode(&data).unwrap();
+    // let args= generate::crateRecode(&data).unwrap();
     let p = ic_cdk::export::Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap();
-    let res:CallResult<()> =  ic_cdk::api::call::call(p, "storage",(&args,)).await;
+    let res:CallResult<()> =  ic_cdk::api::call::call(p, "storage",(&data,)).await;
 }
 
 
--- src/metadata.rs
+++ src/metadata.rs
@@ -1,22 +1,19 @@
-use ic_cdk::{
-    export::Principal
-};
-
-use serde::{Deserialize, Serialize};
+use ic_cdk::{export::Principal};
+use ic_cdk::export::candid::{Deserialize,Nat};
 use ic_cdk::export::candid::CandidType;
-#[derive(Serialize, Deserialize, Debug,Clone,CandidType)]
+#[derive( Deserialize, Debug,Clone,CandidType)]
 pub struct Metadata {
     pub canister:String,
     pub caller: String,
-    pub fee: u64,
+    pub fee: Nat,
     pub method_name: String,
-    pub transaction_time: u64,
-    pub stable_size: u64,
+    pub transaction_time: Nat,
+    pub stable_size: Nat,
 }
 
 
 impl  Metadata {
-    pub fn new(canister:&Principal,caller: &Principal, transaction_time: u64, stable_size: u64,fee:u64,method_name:&str) -> Metadata {
+    pub fn new(canister:&Principal,caller: &Principal, transaction_time: Nat, stable_size: Nat,fee:Nat,method_name:&str) -> Metadata {
         Metadata {
             canister:canister.to_text(),
             caller: caller.to_text(),
