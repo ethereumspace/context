cf28af18c7c31bc86601e76a54eefc0a3ba0f25c
ethereum <1532381694@qq.com>
2021-10-26 14:44:17 +0800

    a

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,8 @@ use ic_cdk::{id,api,api::{stable, time,canister_balance},caller,print};
 use ic_cdk::api::call::{ CallResult};
 
 #[inline(always)]
-pub async fn emit() -> () {
+pub async fn emit() -> CallResult<()> {
     // let name = ic_cdk::api::call::method_name(); 
-
     let canister = id();
     let caller = caller();
     let transaction_time = time();
@@ -21,4 +20,5 @@ pub async fn emit() -> () {
     let data = Metadata::new(&canister,&caller, transaction_time.into(), stable_size.into(),fee.into(),"test");
     let p = Principal::from_text(STORAGECANISTER).unwrap();
     let res:CallResult<()> =  ic_cdk::api::call::call(p,CREATETRANSACTION,(&data,)).await;
+    return res;
 }
