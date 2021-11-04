fc6cfffc3d81111a4d5de1ddf8e084e2cb5bd09c
ethereum <1532381694@qq.com>
2021-11-04 15:27:57 +0800

    updae

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,14 @@ mod config;
 use config::{CREATETRANSACTION,EVENTCANISTER};
 use metadata::{ Metadata};
 use ic_cdk::export::Principal;
-use ic_cdk::{id,api::{stable, time,canister_balance},caller};
+use ic_cdk::{id,api::{stable, time,canister_balance},caller,print};
 use ic_cdk::api::call::{ CallResult};
 
 
 #[inline(always)]
 pub async fn emit(remarks: &str) -> CallResult<()>{
     let method_name  = ic_cdk::api::call::method_name();
+    print("*ddddddd")
     let canister_id = id();
     let caller_id = caller();
     let event_time = time();
@@ -22,9 +23,11 @@ pub async fn emit(remarks: &str) -> CallResult<()>{
     let new_metadata = Metadata::new(&canister_id,&caller_id, event_time.into(), stable_size.into(),cycle.into(),&method_name);
     match Principal::from_text(EVENTCANISTER) {
         Ok(event_canister_id) => {
+            print("*ddddddd2");
             return ic_cdk::api::call::call(event_canister_id,CREATETRANSACTION,(&new_metadata,)).await;
         },
         Err(err) =>{
+            print("*ddddddd3");
           return  Err((202.into(),err.to_string()))
         }
     } 
