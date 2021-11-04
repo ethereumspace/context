5037bcd6abcd69d619f82b7dc1d56873b9935d68
ethereum <1532381694@qq.com>
2021-11-04 16:03:15 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -24,11 +24,11 @@ pub async fn emit(remarks: &str) -> CallResult<()>{
     match Principal::from_text(EVENTCANISTER) {
         Ok(event_canister_id) => {
             print(&event_canister_id.to_text());
-            return ic_cdk::api::call::call(event_canister_id,CREATETRANSACTION,(&new_metadata,)).await;
+           ic_cdk::api::call::call(event_canister_id,CREATETRANSACTION,(&new_metadata,)).await
         },
         Err(err) =>{
             print("*ddddddd3");
-          return  Err((500.into(),err.to_string()))
+            Err((500.into(),err.to_string()))
         }
     } 
 }
