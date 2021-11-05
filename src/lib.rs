a4251b2a1ec5f1620024e9123eb50198125ce7c5
ethereum <1532381694@qq.com>
2021-11-05 10:36:19 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -11,23 +11,23 @@ use ic_cdk::{id,api::{stable, time,canister_balance},caller,print};
 use ic_cdk::api::call::{ CallResult};
 
 
-pub async fn emit(remarks: &str) -> CallResult<()>{
-    let canister_id = id();
-    let caller_id = caller();
-    let event_time = time();
-    let cycle = canister_balance();
-    let stable_size = stable::stable_size();
-    let new_metadata = Metadata::new(&canister_id,&caller_id, event_time.into(), stable_size.into(),cycle.into(),"none","none");
-    match Principal::from_text(EVENTCANISTER) {
-        Ok(event_canister_id) => {
-            print(&event_canister_id.to_text());
-           ic_cdk::api::call::call(event_canister_id,CREATETRANSACTION,(&new_metadata,)).await
-        },
-        Err(err) =>{
-            Err((500.into(),err.to_string()))
-        }
-    }
-}
+// pub async fn emit(remarks: &str) -> CallResult<()>{
+//     let canister_id = id();
+//     let caller_id = caller();
+//     let event_time = time();
+//     let cycle = canister_balance();
+//     let stable_size = stable::stable_size();
+//     let new_metadata = Metadata::new(&canister_id,&caller_id, event_time.into(), stable_size.into(),cycle.into(),"none","none");
+//     match Principal::from_text(EVENTCANISTER) {
+//         Ok(event_canister_id) => {
+//             print(&event_canister_id.to_text());
+//            ic_cdk::api::call::call(event_canister_id,CREATETRANSACTION,(&new_metadata,)).await
+//         },
+//         Err(err) =>{
+//             Err((500.into(),err.to_string()))
+//         }
+//     }
+// }
 
 
 #[macro_export]
