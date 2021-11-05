a63b0193cded738318392bc1965f489b87abd35a
ethereum <1532381694@qq.com>
2021-11-05 10:27:41 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -12,23 +12,35 @@ use ic_cdk::api::call::{ CallResult};
 
 
 pub async fn emit(remarks: &str) -> CallResult<()>{
-    let method_name  = ic_cdk::api::call::method_name();
-    print("*ddddddd");
     let canister_id = id();
     let caller_id = caller();
     let event_time = time();
     let cycle = canister_balance();
     let stable_size = stable::stable_size();
-    let new_metadata = Metadata::new(&canister_id,&caller_id, event_time.into(), stable_size.into(),cycle.into(),&method_name);
+    let new_metadata = Metadata::new(&canister_id,&caller_id, event_time.into(), stable_size.into(),cycle.into(),"none");
     match Principal::from_text(EVENTCANISTER) {
         Ok(event_canister_id) => {
             print(&event_canister_id.to_text());
            ic_cdk::api::call::call(event_canister_id,CREATETRANSACTION,(&new_metadata,)).await
         },
         Err(err) =>{
-            print("*ddddddd3");
             Err((500.into(),err.to_string()))
         }
     }
 }
 
+
+#[macro_export]
+macro_rules! emit{
+($method_name:expr,$argus:expr) =>{
+    let method = $method_name.to_string();
+    println!("method{}",&method);
+    let canister_id = id();
+    let caller_id = caller();
+    let event_time = time();
+    let cycle = canister_balance();
+    let stable_size = stable::stable_size();
+    let new_metadata = Metadata::new(&canister_id,&caller_id, event_time.into(), stable_size.into(),cycle.into(),&method);
+}
+   
+}
